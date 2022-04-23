c98a74f4a5f2b48fef4a86b9cdf079221f0600e1
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-04-23 11:28:46 +0900

    add: support basePath for dev.

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,3 +1,7 @@
+console.log(`NODE_ENV:`, process.env.NODE_ENV);
+
+const isDev = () => process.env.NODE_ENV === "development";
+
 module.exports = {
-  basePath: "/nextjs",
+  basePath: isDev() ? undefined : "/nextjs",
 };
